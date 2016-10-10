04ee8d1ba594b03f6cc67f2f9c03c9bbf3cbbfde
solarspecter315 <ted563473@gmail.com>
2016-10-10 14:02:24 +0800

    upgrade to pug

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.disable('x-powered-by');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
