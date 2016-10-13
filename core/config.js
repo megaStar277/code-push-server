a15df8f2e2e66cb4452b7dd5fb403420262f6c8c
solarspecter315 <ted563473@gmail.com>
2016-10-13 22:44:57 +0800

    fix config

--- core/config.js
+++ core/config.js
@@ -1,5 +1,7 @@
 var env       = process.env.NODE_ENV || 'development';
-var CONFIG_FILE = process.env.CONFIG_FILE || __dirname + '/../config/config.js';
-var _         = require('lodash');
-var config    = _.get(require(CONFIG_FILE), env);
+var CONFIG_FILE = process.env.CONFIG_FILE || './config/config.js';
+var path = require('path');
+var _    = require('lodash');
+var CONFIG_PATH = path.join(__dirname, path.relative(__dirname, CONFIG_FILE));
+var config    = _.get(require(CONFIG_PATH), env);
 module.exports = config;
