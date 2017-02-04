60071c540c6b420b0c36937d49415164f7dec4cd
solarspecter315 <ted563473@gmail.com>
2017-02-04 20:02:54 +0800

    fix version

--- sql/codepush-v0.2.15.sql
+++ sql/codepush-v0.2.15.sql
@@ -2,3 +2,4 @@ ALTER TABLE `deployments_versions` DROP INDEX idx_did_appversion, ADD INDEX `idx
 ALTER TABLE `packages` ADD `deleted_at` TIMESTAMP  NULL;
 ALTER TABLE `packages_metrics` DROP INDEX `udx_packageid`,ADD INDEX `idx_packageid` (`package_id`),ADD `deleted_at` TIMESTAMP  NULL;
 ALTER TABLE `packages_diff` ADD `deleted_at` TIMESTAMP  NULL;
+UPDATE `versions` SET `version` = '0.2.15' WHERE `type` = '1';
