d207b4c8c6b4b6466a99a5fc24ee67000cf03828
solarspecter315 <ted563473@gmail.com>
2017-02-05 16:00:44 +0800

    fix 0.2.14 sql error

--- sql/codepush-v0.2.15.sql
+++ sql/codepush-v0.2.15.sql
@@ -9,6 +9,18 @@ ALTER TABLE `packages_metrics` CHANGE `created_at` `created_at` TIMESTAMP  NULL;
 ALTER TABLE `user_tokens` CHANGE `expires_at` `expires_at` TIMESTAMP  NULL,CHANGE `created_at` `created_at` TIMESTAMP  NULL;
 ALTER TABLE `users` CHANGE `created_at` `created_at` TIMESTAMP  NULL;
 
+DROP PROCEDURE IF EXISTS schema_change;
+CREATE PROCEDURE schema_change()
+BEGIN
+DECLARE  CurrentDatabase VARCHAR(100);
+SELECT DATABASE() INTO CurrentDatabase;
+IF NOT EXISTS (SELECT * FROM information_schema.columns WHERE table_schema=CurrentDatabase AND table_name = 'packages' AND column_name = 'is_mandatory') THEN
+    ALTER TABLE `packages` ADD `is_mandatory` TINYINT(3)  UNSIGNED  NOT NULL  DEFAULT '0';
+END IF;
+END;
+CALL schema_change();
+DROP PROCEDURE IF EXISTS schema_change;
+
 ALTER TABLE `deployments_versions` DROP INDEX idx_did_appversion, ADD INDEX `idx_did_appversion` (`deployment_id`, `app_version`),ADD `deleted_at` TIMESTAMP  NULL;
 ALTER TABLE `packages` ADD `deleted_at` TIMESTAMP  NULL;
 ALTER TABLE `packages_metrics` DROP INDEX `udx_packageid`,ADD INDEX `idx_packageid` (`package_id`),ADD `deleted_at` TIMESTAMP  NULL;
